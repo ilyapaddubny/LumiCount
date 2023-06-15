63acfe7a63930289e7bbd1706a3800e0122e2852
Ilya Paddubny <ilyapoddubny@gmail.com>
2023-06-15 22:13:39 +0300

    Initial Commit

---
+++ SwiftCount/SwiftCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCountApp.swift
+//  SwiftCount
+//
+//  Created by Ilya Paddubny on 15.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
