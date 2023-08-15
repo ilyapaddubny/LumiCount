7382736abdab562e58c894942a159cb3e6616d4e
Ilya Paddubny <ilyapaddubny@gmail.com>
2023-08-15 19:59:00 +0300

    All errors have been processed.

---
+++ LumiCount/Core/Custom views/ErrorView.swift
@@ -0,0 +1,18 @@
+//
+//  ErrorView.swift
+//  LumiCount
+//
+//  Created by Ilya Paddubny on 05.08.2023.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ErrorView()
+}
