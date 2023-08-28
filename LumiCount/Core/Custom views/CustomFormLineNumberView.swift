7dc06f160c0cb248f9d115ab801f078f55795981
Ilya Paddubny <ilyapaddubny@gmail.com>
2023-08-28 22:53:26 +0300

    Beta release

---
+++ LumiCount/Core/Custom views/CustomFormLineNumberView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomFormLineNumberView.swift
+//  LumiCount
+//
+//  Created by Ilya Paddubny on 19.08.2023.
+//
+
+import SwiftUI
+
+struct CustomFormLineNumberView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomFormLineNumberView()
+}
---
+++ LumiCount/Core/Custom views/ExampleGoalRowView.swift
@@ -0,0 +1,18 @@
+//
+//  ExampleGoalRowView.swift
+//  LumiCount
+//
+//  Created by Ilya Paddubny on 24.08.2023.
+//
+
+import SwiftUI
+
+struct ExampleGoalRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExampleGoalRowView()
+}
